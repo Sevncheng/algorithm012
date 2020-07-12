d14b933591e0337278cf5d082edf751360f4dc65
chengyan <sevncy@163.com>
2020-07-12 23:22:15 +0800

    feat: add删除排序数组中的重复项, add旋转数组

---
+++ Week_01/homework/src/main/java/com/base/sevn/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,29 @@
+package com.base.sevn;
+
+import org.junit.jupiter.api.Test;
+
+public class RemoveDuplicatesFromSortedArray {
+
+    /**
+     * 题目理解比较费劲，有两点注意点
+     * 第一：返回值为移除数组的长度
+     * 第二：原数组使用引用方式，并更改原数组开头内容为实际上去重之后的结果
+     * @param nums
+     * @return
+     */
+    public int removeDuplicates(int[] nums) {
+        // 前置判定数组为空或者长度等于0 直接返回
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int first = 0;
+        // 循环主体为尾部指针
+        for (int last = first + 1; last < nums.length; last++) {
+            // 如果不相等，则将头部指针前移后赋值为当前尾部指针值
+            if (nums[first] != nums[last]) {
+                nums[++first] = nums[last];
+            }
+        }
+        return first + 1;
+    }
+}
---
+++ Week_01/homework/src/main/java/com/base/sevn/RotateArray.java
@@ -0,0 +1,42 @@
+package com.base.sevn;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+public class RotateArray {
+
+    /**
+     * tips: 这里数组的实际挪动位置为 期望长度 / 实际数组长度 取余数
+     * 方法1：直接暴力破解 时间复杂度n^2 双重循环每次步长都是1
+     * 方法2：增加新数组直接存放目标值
+     * 方法3：环状替换TODO
+     * 方法4：反转
+     * @param nums
+     * @param k
+     */
+    public void rotate(int[] nums, int k) {
+        if (nums == null || nums.length < 1 || k < 1) {
+            return;
+        }
+        int actualRemove = k % nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, actualRemove - 1);
+        reverse(nums, actualRemove, nums.length - 1);
+    }
+
+    public void reverse(int nums[], int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            end--;
+            start++;
+        }
+    }
+
+    @Test
+    public void test() {
+        rotate(new int[]{1, 2, 3, 4, 5, 6, 7}, 3);
+    }
+}
