d737686ffdd8a26ffca16bdbbad015a378163720
chengyan <chengyan@tmxmall.com>
2020-07-20 20:31:21 +0800

    feat: 代码增加

--- Week_01/homework/src/main/java/com/base/sevn/MyCircularDeque.java
+++ Week_01/homework/src/main/java/com/base/sevn/MyCircularDeque.java
@@ -5,52 +5,95 @@ public class MyCircularDeque {
     /**
      * 存储的基础还是数组
      */
-    private int[] arr;
-    private int size;
+    int[] arr;
+    int size;
+    int head;
+    int rear;
 
     /** Initialize your data structure here. Set the size of the deque to be k. */
     public MyCircularDeque(int k) {
-        int[] arr = new int[k];
-        size = k;
+        if (k < 0) {
+            throw new IllegalArgumentException("Deque size is less than 0!");
+        }
+        this.arr = new int[k];
+        this.size = 0;
+        this.head = 0;
+        this.rear = 0;
     }
 
-    /** Adds an item at the front of Deque. Return true if the operation is successful. */
+    /** Adds an item at the head of Deque. Return true if the operation is successful. */
     public boolean insertFront(int value) {
-
+        if (size == arr.length) {
+            return false;
+        }
+        if (head == 0) {
+            arr[arr.length - 1] = value;
+            head = arr.length - 1;
+        } else {
+            arr[--head] = value;
+        }
+        size ++;
+        return true;
     }
 
     /** Adds an item at the rear of Deque. Return true if the operation is successful. */
     public boolean insertLast(int value) {
-
+        if (size == arr.length) {
+            return false;
+        }
+        arr[rear] = value;
+        rear = rear == arr.length - 1 ? 0 : rear + 1;
+        size ++;
+        return true;
     }
 
-    /** Deletes an item from the front of Deque. Return true if the operation is successful. */
+    /** Deletes an item from the head of Deque. Return true if the operation is successful. */
     public boolean deleteFront() {
-
+        if (size == 0) {
+            return false;
+        }
+        head = head == arr.length - 1 ? 0 : head + 1;
+        size--;
+        return true;
     }
 
     /** Deletes an item from the rear of Deque. Return true if the operation is successful. */
     public boolean deleteLast() {
-
+        if (size == 0) {
+            return false;
+        }
+        if (rear == 0) {
+            rear = arr.length - 1;
+        } else {
+            rear--;
+        }
+        size --;
+        return true;
     }
 
-    /** Get the front item from the deque. */
-    public int getFront() {
-        
+    /** Get the head item from the deque. */
+    public int getHead() {
+        if (size == 0) {
+            return -1;
+        }
+        return arr[head];
     }
 
-    /** Get the last item from the deque. */
+    /** Get the rear item from the deque. */
     public int getRear() {
-
+        if (size == 0) {
+            return -1;
+        }
+        return rear == 0 ? arr[arr.length - 1] : arr[rear - 1];
     }
 
     /** Checks whether the circular deque is empty or not. */
     public boolean isEmpty() {
-        return arr.length == 0 || arr == null;
+        return size == 0;
     }
 
     /** Checks whether the circular deque is full or not. */
     public boolean isFull() {
-        return arr.length == size;
+        return size == arr.length;
     }
 }
---
+++ Week_01/homework/src/main/java/com/base/sevn/TrappingRainWater.java
@@ -0,0 +1,21 @@
+package com.base.sevn;
+
+public class TrappingRainWater {
+
+    /**
+     * 题目到手两个思路, 暴力和双指针, 但是题目理解起来还是比较费劲
+     * 方法1: 暴力
+     * @param height
+     * @return
+     */
+    public int trap(int[] height) {
+        int area = 0;
+        for (int i = 0; i < height.length; i++) {
+
+
+
+
+        }
+        return area;
+    }
+}
