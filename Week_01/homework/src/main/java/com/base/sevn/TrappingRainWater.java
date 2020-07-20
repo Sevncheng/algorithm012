66ea94b20247ec9546b6d9f7639693c1e4767ad9
chengyan <sevncy@163.com>
2020-07-20 23:16:34 +0800

    feat: 接雨水

--- Week_01/homework/src/main/java/com/base/sevn/TrappingRainWater.java
+++ Week_01/homework/src/main/java/com/base/sevn/TrappingRainWater.java
@@ -1,21 +1,35 @@
 package com.base.sevn;
 
+import org.junit.jupiter.api.Test;
+
 public class TrappingRainWater {
 
     /**
-     * 题目到手两个思路, 暴力和双指针, 但是题目理解起来还是比较费劲
+     * 题目到手两个思路, 暴力和双指针, 但是题目理解起来还是比较费劲, 双指针一开始也没有思路, 使用了动态题解的动态规划
      * 方法1: 暴力
+     * 方法2: 动态规划
      * @param height
      * @return
      */
     public int trap(int[] height) {
         int area = 0;
-        for (int i = 0; i < height.length; i++) {
-
-
-
-
+        for (int i = 1; i < height.length - 1; i++) {
+            int leftMax = 0;
+            for (int j = 0; j < i; j++) {
+                leftMax = Math.max(leftMax, height[j]);
+            }
+            int rightMax = 0;
+            for (int j = i; j < height.length; j++) {
+                rightMax = Math.max(rightMax, height[j]);
+            }
+            int result = Math.min(leftMax, rightMax) - height[i];
+            area += result > 0 ? result : 0;
         }
         return area;
+    }
+
+    @Test
+    public void test() {
+        trap(new int[]{2, 0 ,2});
     }
 }
